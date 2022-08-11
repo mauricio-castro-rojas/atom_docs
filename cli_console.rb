4d9858365e22646c8b418b877b31d8abda74b170
Mauricio Castro <mauricio.castro@symplifica.com>
2022-08-10 21:58:31 -0500

    update 2

--- cli_console.rb
+++ cli_console.rb
@@ -8,3 +8,8 @@ id es el id en la tabla Admin
 
 ["fernando.ricaurte@symplifica.com".size, 5, Time.zone.now.day].join("23")
 => "32235238"
+
+["mauricio.castro@symplifica.com".size, 5, Time.zone.now.day].join("23")
+
+validar la facturacion
+validar la Pila si ya tiene novedad de retiro , que hacer en esos casos
